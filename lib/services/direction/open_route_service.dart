df7a60f12e77be6e2795a0fdbf6cef41d62b0a0e
behong1999 <khonhong46632@gmail.com>
2023-05-24 12:21:54 +0300

    minor

--- lib/services/direction/open_route_service.dart
+++ lib/services/direction/open_route_service.dart
@@ -33,14 +33,14 @@ Future<List<LatLng>> getPolylineCoordinates({
   required double endLng,
 }) async {
   try {
-    final json = await getData(
+    final jsonData = await getData(
       startLat: startLat,
       startLng: startLng,
       endLat: endLat,
       endLng: endLng,
     );
 
-    List coordinates = json['features'][0]['geometry']['coordinates'];
+    List coordinates = jsonData['features'][0]['geometry']['coordinates'];
 
     //NOTE: In the OpenRouteService API, the first value is longitude (lng)
     // and the second value is latitude (lat).
